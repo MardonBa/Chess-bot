c477fe86cc9237cd6e404939364a6f84cfdb6b5c
Bayan Mardon <mardon.junior@gmail.com>
2023-01-24 12:17:47 -0500

    Update main.py

--- source files/main.py
+++ source files/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 start = datetime.now()
-import board_and_pieces as bd_pc
-import move_pieces as mp
+import board_and_pieces_test as bd_pc
+import move_pieces_test as mp
 import pygame as p
 from sys import exit
 
@@ -64,103 +64,88 @@ def squares_init():
 squares_dict = squares_init()
 
 # The following lines initalize and draw the pieces in their starting squares
+
 def pieces_init():
     pieces_dict = {}
 
     white_rook1 = bd_pc.White_Rook(square_placement_dict["A1"][0], square_placement_dict["A1"][1], "A1")
     white_rook1_group = bd_pc.ExtendedGroupSingle()       # When a piece gets taken, can the group get changed to None, and the piece goes away?
     white_rook1_group.add(white_rook1)
-    white_rook1_group.draw(screen)
     pieces_dict["A1"] = white_rook1_group
 
     white_knight1 = bd_pc.White_Knight(square_placement_dict["B1"][0], square_placement_dict["B1"][1], "B1")
     white_knight1_group = bd_pc.ExtendedGroupSingle()
     white_knight1_group.add(white_knight1)
-    white_knight1_group.draw(screen)
     pieces_dict["B1"] = white_knight1_group
 
     white_bishop1 = bd_pc.White_Bishop(square_placement_dict["C1"][0], square_placement_dict["C1"][1], "C1")
     white_bishop1_group = bd_pc.ExtendedGroupSingle()
     white_bishop1_group.add(white_bishop1)
-    white_bishop1_group.draw(screen)
     pieces_dict["C1"] = white_bishop1_group
 
     white_queen = bd_pc.White_Queen(square_placement_dict["D1"][0], square_placement_dict["D1"][1], "D1")
     white_queen_group = bd_pc.ExtendedGroupSingle()
     white_queen_group.add(white_queen)
-    white_queen_group.draw(screen)
     pieces_dict["D1"] = white_queen_group
 
     white_king = bd_pc.White_King(square_placement_dict["E1"][0], square_placement_dict["E1"][1], "E1")
     white_king_group = bd_pc.ExtendedGroupSingle()
     white_king_group.add(white_king)
-    white_king_group.draw(screen)
     pieces_dict["E1"] = white_king_group
 
     white_bishop2 = bd_pc.White_Bishop(square_placement_dict["F1"][0], square_placement_dict["F1"][1], "F1")
     white_bishop2_group = bd_pc.ExtendedGroupSingle()
     white_bishop2_group.add(white_bishop2)
-    white_bishop2_group.draw(screen)
     pieces_dict["F1"] = white_bishop2_group
 
     white_knight2 = bd_pc.White_Knight(square_placement_dict["G1"][0], square_placement_dict["G1"][1], "G1")
     white_knight2_group = bd_pc.ExtendedGroupSingle()
     white_knight2_group.add(white_knight2)
-    white_knight2_group.draw(screen)
     pieces_dict["G1"] = white_knight2_group
 
     white_rook2 = bd_pc.White_Rook(square_placement_dict["H1"][0], square_placement_dict["H1"][1], "H1")
     white_rook2_group = bd_pc.ExtendedGroupSingle()
     white_rook2_group.add(white_rook2)
-    white_rook2_group.draw(screen)
     pieces_dict["H1"] = white_rook2_group
 
     white_pawn1 = bd_pc.White_Pawn(square_placement_dict["A2"][0], square_placement_dict["A2"][1], "A2")
     white_pawn1_group = bd_pc.ExtendedGroupSingle()
     white_pawn1_group.add(white_pawn1)
-    white_pawn1_group.draw(screen)
     pieces_dict["A2"] = white_pawn1_group
 
     white_pawn2 = bd_pc.White_Pawn(square_placement_dict["B2"][0], square_placement_dict["B2"][1], "B2")
     white_pawn2_group = bd_pc.ExtendedGroupSingle()
     white_pawn2_group.add(white_pawn2)
-    white_pawn2_group.draw(screen)
     pieces_dict["B2"] = white_pawn2_group
 
     white_pawn3 = bd_pc.White_Pawn(square_placement_dict["C2"][0], square_placement_dict["C2"][1], "C2")
     white_pawn3_group = bd_pc.ExtendedGroupSingle()
     white_pawn3_group.add(white_pawn3)
-    white_pawn3_group.draw(screen)
     pieces_dict["C2"] = white_pawn3_group
 
     white_pawn4 = bd_pc.White_Pawn(square_placement_dict["D2"][0], square_placement_dict["D2"][1], "D2")
     white_pawn4_group = bd_pc.ExtendedGroupSingle()
     white_pawn4_group.add(white_pawn4)
-    white_pawn4_group.draw(screen)
     pieces_dict["D2"] = white_pawn4_group
 
     white_pawn5 = bd_pc.White_Pawn(square_placement_dict["E2"][0], square_placement_dict["E2"][1], "E2")
     white_pawn5_group = bd_pc.ExtendedGroupSingle()
     white_pawn5_group.add(white_pawn5)
-    white_pawn5_group.draw(screen)
     pieces_dict["E2"] = white_pawn5_group
 
     white_pawn6 = bd_pc.White_Pawn(square_placement_dict["F2"][0], square_placement_dict["F2"][1], "F2")
     white_pawn6_group = bd_pc.ExtendedGroupSingle()
     white_pawn6_group.add(white_pawn6)
-    white_pawn6_group.draw(screen)
     pieces_dict["F2"] = white_pawn6_group
 
     white_pawn7 = bd_pc.White_Pawn(square_placement_dict["G2"][0], square_placement_dict["G2"][1], "G2")
     white_pawn7_group = bd_pc.ExtendedGroupSingle()
     white_pawn7_group.add(white_pawn7)
-    white_pawn7_group.draw(screen)
     pieces_dict["G2"] = white_pawn7_group
 
     white_pawn8 = bd_pc.White_Pawn(square_placement_dict["H2"][0], square_placement_dict["H2"][1], "H2")
     white_pawn8_group = bd_pc.ExtendedGroupSingle()
     white_pawn8_group.add(white_pawn8)
-    white_pawn8_group.draw(screen)
     pieces_dict["H2"] = white_pawn8_group
 
 
@@ -168,103 +153,94 @@ def pieces_init():
     black_rook1 = bd_pc.Black_Rook(square_placement_dict["A8"][0], square_placement_dict["A8"][1], "A8")
     black_rook1_group = bd_pc.ExtendedGroupSingle()       # When a piece gets taken, can the group get changed to None, and the piece goes away?
     black_rook1_group.add(black_rook1)
-    black_rook1_group.draw(screen)
     pieces_dict["A8"] = black_rook1_group
 
     black_knight1 = bd_pc.Black_Knight(square_placement_dict["B8"][0], square_placement_dict["B8"][1], "B8")
     black_knight1_group = bd_pc.ExtendedGroupSingle()
     black_knight1_group.add(black_knight1)
-    black_knight1_group.draw(screen)
     pieces_dict["B8"] = black_knight1_group
 
     black_bishop1 = bd_pc.Black_Bishop(square_placement_dict["C8"][0], square_placement_dict["C8"][1], "C8")
     black_bishop1_group = bd_pc.ExtendedGroupSingle()
     black_bishop1_group.add(black_bishop1)
-    black_bishop1_group.draw(screen)
     pieces_dict["C8"] = black_bishop1_group
 
     black_queen = bd_pc.Black_Queen(square_placement_dict["D8"][0], square_placement_dict["D8"][1], "D8")
     black_queen_group = bd_pc.ExtendedGroupSingle()
     black_queen_group.add(black_queen)
-    black_queen_group.draw(screen)
     pieces_dict["D8"] = black_queen_group
 
     black_king = bd_pc.Black_King(square_placement_dict["E8"][0], square_placement_dict["E8"][1], "E8")
     black_king_group = bd_pc.ExtendedGroupSingle()
     black_king_group.add(black_king)
-    black_king_group.draw(screen)
     pieces_dict["E8"] = black_king_group
 
     black_bishop2 = bd_pc.Black_Bishop(square_placement_dict["F8"][0], square_placement_dict["F8"][1], "F8")
     black_bishop2_group = bd_pc.ExtendedGroupSingle()
     black_bishop2_group.add(black_bishop2)
-    black_bishop2_group.draw(screen)
     pieces_dict["F8"] = black_bishop2_group
 
 
     black_knight2 = bd_pc.Black_Knight(square_placement_dict["G8"][0], square_placement_dict["G8"][1], "G8")
     black_knight2_group = bd_pc.ExtendedGroupSingle()
     black_knight2_group.add(black_knight2)
-    black_knight2_group.draw(screen)
     pieces_dict["G8"] = black_knight2_group
 
     black_rook2 = bd_pc.Black_Rook(square_placement_dict["H8"][0], square_placement_dict["H8"][1], "H8")
     black_rook2_group = bd_pc.ExtendedGroupSingle()
     black_rook2_group.add(black_rook2)
-    black_rook2_group.draw(screen)
     pieces_dict["H8"] = black_rook2_group
 
     black_pawn1 = bd_pc.Black_Pawn(square_placement_dict["A7"][0], square_placement_dict["A7"][1], "A7")
     black_pawn1_group = bd_pc.ExtendedGroupSingle()
     black_pawn1_group.add(black_pawn1)
-    black_pawn1_group.draw(screen)
     pieces_dict["A7"] = black_pawn1_group
 
     black_pawn2 = bd_pc.Black_Pawn(square_placement_dict["B7"][0], square_placement_dict["B7"][1], "B7")
     black_pawn2_group = bd_pc.ExtendedGroupSingle()
     black_pawn2_group.add(black_pawn2)
-    black_pawn2_group.draw(screen)
     pieces_dict["B7"] = black_pawn2_group
 
     black_pawn3 = bd_pc.Black_Pawn(square_placement_dict["C7"][0], square_placement_dict["C7"][1], "C7")
     black_pawn3_group = bd_pc.ExtendedGroupSingle()
     black_pawn3_group.add(black_pawn3)
-    black_pawn3_group.draw(screen)
     pieces_dict["C7"] = black_pawn3_group
 
     black_pawn4 = bd_pc.Black_Pawn(square_placement_dict["D7"][0], square_placement_dict["D7"][1], "D7")
     black_pawn4_group = bd_pc.ExtendedGroupSingle()
     black_pawn4_group.add(black_pawn4)
-    black_pawn4_group.draw(screen)
     pieces_dict["D7"] = black_pawn4_group
 
     black_pawn5 = bd_pc.Black_Pawn(square_placement_dict["E7"][0], square_placement_dict["E7"][1], "E7")
     black_pawn5_group = bd_pc.ExtendedGroupSingle()
     black_pawn5_group.add(black_pawn5)
-    black_pawn5_group.draw(screen)
     pieces_dict["E7"] = black_pawn5_group
 
     black_pawn6 = bd_pc.Black_Pawn(square_placement_dict["F7"][0], square_placement_dict["F7"][1], "F7")
     black_pawn6_group = bd_pc.ExtendedGroupSingle()
     black_pawn6_group.add(black_pawn6)
-    black_pawn6_group.draw(screen)
     pieces_dict["F7"] = black_pawn6_group
 
     black_pawn7 = bd_pc.Black_Pawn(square_placement_dict["G7"][0], square_placement_dict["G7"][1], "G7")
     black_pawn7_group = bd_pc.ExtendedGroupSingle()
     black_pawn7_group.add(black_pawn7)
-    black_pawn7_group.draw(screen)
     pieces_dict["G7"] = black_pawn7_group
 
     black_pawn8 = bd_pc.Black_Pawn(square_placement_dict["H7"][0], square_placement_dict["H7"][1], "H7")
     black_pawn8_group = bd_pc.ExtendedGroupSingle()
     black_pawn8_group.add(black_pawn8)
-    black_pawn8_group.draw(screen)
     pieces_dict["H7"] = black_pawn8_group
 
     return pieces_dict
 
 pieces_dict = pieces_init()
+
+
+def pieces_draw(pieces_dict):
+    for val in pieces_dict.values():
+        val.draw(screen)
+
+pieces_draw(pieces_dict)
 
 selected_piece = None
 selected_square = None
@@ -288,7 +264,8 @@ while True:
                 selected_square = mp.select_square(squares_dict)
                 print(f"You selected {selected_square}")
                 selected_piece.change_piece_coordinates(square_placement_dict[selected_square][0], square_placement_dict[selected_square][1])
-                selected_piece.draw(screen)
+                squares_init()
+                pieces_draw(pieces_dict)
                 selected_square = None
                 selected_piece = None
 
@@ -296,8 +273,7 @@ while True:
             print("mouse button up")
         
 
-    #squares_init()     uncomment later
-    #pieces_init()       # change pieces_init() to take in input for what piece, group, etc...       should have default args so that the first call is the starting position, then it can be recalled in the game loop drawing pieces in new spots if needed
+
     p.display.update()
 
     clock.tick(max_fps)
