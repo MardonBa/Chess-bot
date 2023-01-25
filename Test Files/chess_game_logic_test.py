ee88d540fce23f5460602d12efcc6f22b0b17926
Bayan Mardon <mardon.junior@gmail.com>
2023-01-25 18:41:58 -0500

    start adding code for determining squares to edge

--- Test Files/chess_game_logic_test.py
+++ Test Files/chess_game_logic_test.py
@@ -1,5 +1,31 @@
 
 
+def squares_to_edge(squares_list, initial_square, direction, diagonal=None):     # diagonal should be paired with up or down with direction, should be right or left
+    # direction should be up, down, left, right
+    
+    square_index = squares_list.index(initial_square)
+    if diagonal == None:
+        if direction == "up":
+            while square_index <= 55:
+                square_index += 8
+                
+            to_edge = square_index
+            return to_edge
+
+        elif direction == "down":
+            while square_index >= 8:
+                square_index -= 8
+            
+            to_edge = square_index
+            return to_edge
+
+        elif direction == "right":
+            to_edge = (square_index + 1) % 8
+            return to_edge
+
+        elif direction == "left":
+            to_edge = square_index % 8
+
 def move_pawn(color, initial_square, squares_list, first_move=False, can_capture_right=False, can_capture_left=False, en_passant=False):
     initial_square_index = squares_list.index(initial_square)
     possible_moves = []
@@ -21,10 +47,15 @@ def move_pawn(color, initial_square, squares_list, first_move=False, can_capture
             possible_moves.append(initial_square_index - 9)
         elif first_move == True:
             possible_moves.append(initial_square_index - 16)
+
+    promote = False         # add code for determining if promotion is true.        squares_to_edge(top) = 1
+    if promote == True:
+        pass
         
     squares_to_move_to = []
     for move in possible_moves:
         squares_to_move_to.append(squares_list[move])
+
 
     return squares_to_move_to
         
